76bc339ef34a66defb689f6f5602954757e62cc6
HieptenHIep <ngochiepcb@gmail.com>
2025-12-02 08:09:05 +0700

    Demo12 part2

--- Assets/Scripts/MissonManager.cs
+++ Assets/Scripts/MissonManager.cs
@@ -8,8 +8,9 @@ public class MissonManager : MonoBehaviour
     public GameFlow gameflow;
     public int repuiredKill;
     public Text missionText;
-
+    private bool isPlayerExit;
     private int currentKill;
+    public Gate gate;
     private void Start()
     {
         StartCoroutine(VeryfiMissions());
@@ -17,6 +18,7 @@ public class MissonManager : MonoBehaviour
     private IEnumerator VeryfiMissions()
     {
         yield return VeryfiZombieKill();
+        yield return VeryfiPlayerExit();
         gameflow.OnMissonCompled();
     }
     private IEnumerator VeryfiZombieKill()
@@ -25,10 +27,20 @@ public class MissonManager : MonoBehaviour
         missionText.text = $"Kill {repuiredKill} zombies ";
         yield return new WaitUntil(() => currentKill >= repuiredKill);
      }
-
+    private IEnumerator VeryfiPlayerExit()
+    {
+        missionText.text = $"Find Exit Door";
+        gate.onPlayerEnter.AddListener(OnPlayerExit);
+         yield return new WaitUntil(()=>isPlayerExit);
+        gate.onPlayerEnter.RemoveListener(OnPlayerExit);
+    }
+    private void OnPlayerExit()
+    {
+        isPlayerExit = true;
+    }
     public void OnZombieKilled(GameObject zombie)
     {
         currentKill++;
     }
-
+   
 }
